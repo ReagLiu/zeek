d9c567eb1e539d851307b49d9f381f9a5afd771c
Tim Wojtulewicz <tim@corelight.com>
2021-10-01 12:54:27 -0700

    Add concept of "parent" tag namespaces
    This allows us to create an EnumType that groups all of the analyzer
    tag values into a single type, while still having the existing types
    that split them up. We can then use this for certain events that benefit
    from taking all of the tag types at once.

--- src/packet_analysis/Manager.cc
+++ src/packet_analysis/Manager.cc
@@ -11,7 +11,8 @@
 
 using namespace zeek::packet_analysis;
 
-Manager::Manager() : plugin::ComponentManager<packet_analysis::Component>("PacketAnalyzer", "Tag")
+Manager::Manager()
+	: plugin::ComponentManager<packet_analysis::Component>("PacketAnalyzer", "Tag", "AllAnalyzers")
 	{
 	}
 
